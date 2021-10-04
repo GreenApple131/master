e8147be7ee5de7ae228829c65e727528f10b221c
GreenApple131 <dimalg400@gmail.com>
2021-10-04 23:11:59 +0300

    Moved map to different component

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,19 +1,43 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 
+/////////////////////////  Reset Marker Icon
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+//////////////////////////////////////////////////////
+
 
 export default function Homepage() {
-    return(
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+    const state = {
+        center: [48.9866, 24.7100],
+        my_home: [48.990707, 24.706905],
+        zoom: 15
+      };
+    
+
+    return (
+        <MapContainer style={{ height: "100vh", width: "100%" }} center={state.center} zoom={state.zoom} scrollWheelZoom={true} >
+        <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            style={{width: "100%", height: "100%"}}
+        />
+        <Marker
+            position={state.my_home}
+            
+        >
+            <Popup>
+            A pretty CSS3 popup. <br /> Center.
+            </Popup>
+        </Marker>
         </MapContainer>
-    )
+    );
 }
